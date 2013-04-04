9b5aea27a14f92ae6340ade77000c815ed7584a2
Philip Whitehouse <philip@whiuk.com>
2013-04-04 22:41:42 +0100

    Added Commons-CLI and done some work on the entry point to take properties file as a CLI option.

--- GameServer/src/com/whiuk/philip/game/server/Main.java
+++ GameServer/src/com/whiuk/philip/game/server/Main.java
@@ -5,8 +5,18 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
+import org.apache.commons.cli.BasicParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 
 import com.whiuk.philip.game.server.GameServer.GameServerProperties;
 
@@ -16,28 +26,77 @@ import com.whiuk.philip.game.server.GameServer.GameServerProperties;
  */
 public class Main {
 
-    private static Logger logger;
-    private static String propertiesFilename = "/etc/opt/philipwhiuk/gameServer.properties";
-    private static File propertiesFile = new File(propertiesFilename);
+	/**
+	 * Utility class - private constructor
+	 */
+	private Main() {
+	}
+
+	/**
+	 *
+	 */
+    private static Logger logger = Logger.getLogger(Main.class);
+    /**
+     *
+     */
+    private static final String defaultPropertiesFilename =
+    		"/etc/opt/philipwhiuk/gameServer.properties";
+    /**
+     *
+     */
+    private static String propertiesFilename = defaultPropertiesFilename;
 
     /**
      * @param args
      * @throws IOException IO exception reading property file
-     * @throws FileNotFoundException Property file not found
      */
-    public static void main(String[] args) throws FileNotFoundException, IOException {
+    public static void main(String[] args)
+    		throws IOException {
+    	//TODO: Initialize logging properly
+    	BasicConfigurator.configure();
+    	CommandLineParser parser = new BasicParser();
+    	Options options = new Options();
+    	Option help = new Option("help", "print this message");
+    	Option properties   = OptionBuilder.withArgName("file")
+                .hasArg()
+                .withDescription(
+            		"use given file to retrieve additional properties")
+                .create("properties");
+		options.addOption(help);
+    	options.addOption(properties);
+    	try {
+            // parse the command line arguments
+            CommandLine line = parser.parse(options, args);
+            if (line.hasOption("help")) {
+            	// automatically generate the help statement
+            	HelpFormatter formatter = new HelpFormatter();
+            	formatter.printHelp("game-server", options);
+            	System.exit(0);
+            }
+            if (line.hasOption("properties")) {
+            	propertiesFilename = line.getOptionValue("properties");
+            }
+        }
+        catch (ParseException e) {
+            // oops, something went wrong
+        	logger.log(Level.WARN,
+        			"Unable to read command line arguments", e);
+        }
+
         GameServerProperties gsProp;
+        File file = new File(propertiesFilename);
         try {
             Properties prop = new Properties();
-            prop.load(new FileReader(propertiesFile));
+            prop.load(new FileReader(file));
             gsProp = new GameServerProperties(prop);
         } catch (Exception e) {
-            logger.log(Level.WARNING, "Error reading properties file: " + propertiesFilename, e);
+        	System.out.println(file.getAbsolutePath());
+            logger.log(Level.WARN,
+            		"Error reading properties file: " + propertiesFilename,
+            		e);
             gsProp = new GameServerProperties();
         }
         GameServer server = new GameServer(gsProp);
-
-
     }
 
 }
