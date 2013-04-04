faac11e805203db3b84c6fea17f3d6601897a4b1
Philip Whitehouse <philip@whiuk.com>
2013-04-04 19:27:01 +0100

    Client initial commit

---
+++ GameClient/src/com/whiuk/philip/game/client/Main.java
@@ -0,0 +1,17 @@
+package com.whiuk.philip.game.client;
+
+/**
+ * @author Philip
+ *
+ */
+public class Main {
+
+    /**
+     * @param args Command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
