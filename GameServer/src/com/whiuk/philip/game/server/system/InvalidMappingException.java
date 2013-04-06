397fb9bef65277659292b27486bb03b178ab56c2
Philip Whitehouse <philip@whiuk.com>
2013-04-07 00:55:41 +0100

    Missing exception from connection management changes.

---
+++ GameServer/src/com/whiuk/philip/game/server/system/InvalidMappingException.java
@@ -0,0 +1,13 @@
+package com.whiuk.philip.game.server.system;
+
+public class InvalidMappingException extends Exception {
+
+	/**
+	 *
+	 * @param string
+	 */
+	public InvalidMappingException(final String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
