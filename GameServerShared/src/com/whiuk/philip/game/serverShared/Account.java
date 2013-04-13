086f87b328ce0d9dc3a36aef65c42bb0026478dc
Philip Whitehouse <philip@whiuk.com>
2013-04-13 15:48:24 +0100

    Refactoring shared classes (part 2)

---
+++ GameServerShared/src/com/whiuk/philip/game/serverShared/Account.java
@@ -0,0 +1,71 @@
+package com.whiuk.philip.game.serverShared;
+
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+/**
+ * @author Philip
+ */
+@Entity
+public class Account {
+
+    /**
+     *
+     */
+    @Id
+    private Long id;
+    /**
+     *
+     */
+    private String username;
+    /**
+     *
+     */
+    @OneToMany(mappedBy = "account")
+    private Set<LoginAttempt> loginAttempts;
+    /**
+     *
+     */
+    private String password;
+
+    /**
+     * @param nanoTime
+     */
+    public final void addLoginAttempt(final LoginAttempt attempt) {
+        loginAttempts.add(attempt);
+    }
+
+    /**
+     * @return
+     */
+    public final String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param p
+     *            password
+     */
+    public final void setPassword(final String p) {
+        password = p;
+    }
+
+    /**
+     * @return
+     */
+    public final String getUsername() {
+        // TODO Auto-generated method stub
+        return username;
+    }
+
+    /**
+     * @param u
+     *            username
+     */
+    public final void setUsername(final String u) {
+        username = u;
+    }
+}
---
+++ GameServerShared/src/com/whiuk/philip/game/serverShared/Connection.java
@@ -0,0 +1,79 @@
+package com.whiuk.philip.game.serverShared;
+
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.whiuk.philip.game.shared.Messages.ClientInfo;
+
+/**
+ * Information about a connection.
+ * 
+ * @author Philip Whitehouse
+ */
+@Entity
+public class Connection {
+    /**
+     * 
+     */
+    @Id
+    private long id;
+    /**
+	 *
+	 */
+    private long lastConnectionTime;
+    /**
+	 *
+	 */
+    private boolean active;
+    /**
+	 *
+	 */
+    private ClientInfo clientInfo;
+    /**
+	 *
+	 */
+    @OneToMany(mappedBy = "connection")
+    private Set<LoginAttempt> loginAttempts;
+
+    /**
+     * Bean constructor.
+     */
+    public Connection() {
+    }
+
+    /**
+     * @param c
+     * @param nanoTime
+     * @param b
+     */
+    public Connection(final ClientInfo c, final long nanoTime, final boolean b) {
+        this.clientInfo = c;
+        this.lastConnectionTime = nanoTime;
+        this.active = b;
+    }
+
+    /**
+	 *
+	 */
+    public final void setActive(final boolean a) {
+        active = a;
+    }
+
+    /**
+     * @param attempt
+     */
+    public final void addLoginAttempt(final LoginAttempt attempt) {
+        this.loginAttempts.add(attempt);
+    }
+
+    /**
+     * @param nanoTime
+     */
+    public final void setLastConnectionTime(final long nanoTime) {
+        lastConnectionTime = nanoTime;
+    }
+
+}
