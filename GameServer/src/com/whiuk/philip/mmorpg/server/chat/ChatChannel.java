4b535cf2d12edb181049399d15314b41d54c078a
Philip Whitehouse <philip@whiuk.com>
2013-07-07 02:12:19 +0100

    Initial work on chat messages server-side.

---
+++ GameServer/src/com/whiuk/philip/mmorpg/server/chat/ChatChannel.java
@@ -0,0 +1,22 @@
+package com.whiuk.philip.mmorpg.server.chat;
+
+import com.whiuk.philip.mmorpg.serverShared.Account;
+
+public class ChatChannel {
+
+    public boolean hasAccountSendPrivilege(Account account) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public boolean hasAccountRegistered(Account account) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public void sendMessage(Account account, String message) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
--- GameServer/src/com/whiuk/philip/mmorpg/server/chat/ChatServiceImpl.java
+++ GameServer/src/com/whiuk/philip/mmorpg/server/chat/ChatServiceImpl.java
@@ -1,5 +1,7 @@
 package com.whiuk.philip.mmorpg.server.chat;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Service;
 
 import com.whiuk.philip.mmorpg.serverShared.Account;
@@ -11,10 +13,40 @@ import com.whiuk.philip.mmorpg.shared.Messages.ClientMessage.ChatData;
 @Service
 public class ChatServiceImpl implements ChatService {
 
-    @Override
-    public void processMessage(final Account account, final ChatData chatData) {
-        // TODO Auto-generated method stub
+    private Map<Integer, ChatChannel> channels;
 
+    @Override
+    public final void processMessage(
+            final Account account, final ChatData chatData) {
+        if (chatData.getPrivate()) {
+            if (chatData.hasTarget()) {
+                handlePrivateMessage(account, chatData.getMessage(),
+                    chatData.getTarget());
+            } else {
+                //Handle invalid message
+            }
+        } else {
+            ChatChannel c = channels.get(chatData.getChannel());
+            if (c != null
+                    && c.hasAccountRegistered(account)
+                    && c.hasAccountSendPrivilege(account)) {
+                c.sendMessage(account, chatData.getMessage());
+            } else {
+                //Handle unauthorised message.
+            }
+        }
+    }
+
+    /**
+     * Handle the sending of a private message
+     * from <code>src</code> to <code>target</code>.
+     * @param src Source of message
+     * @param message Message contents
+     * @param target Target
+     */
+    private void handlePrivateMessage(Account src, String message,
+            String target) {
+        // TODO Auto-generated method stub
     }
 
     @Override
