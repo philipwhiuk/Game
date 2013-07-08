1c98aff0ef17f5d0788eb1ea0e3599aa1c5a18fd
Philip Whitehouse <philip@whiuk.com>
2013-07-08 23:53:20 +0100

    More AI project based merging and doc.

--- GameServer/src/com/whiuk/philip/mmorpg/server/game/Zone.java
+++ GameServer/src/com/whiuk/philip/mmorpg/server/game/Zone.java
@@ -1,19 +1,27 @@
 package com.whiuk.philip.mmorpg.server.game;
 
+import java.util.List;
+
 /**
  * Zone.
  * @author Philip
  *
  */
 public class Zone {
+    /**
+     * 
+     */
+    private List<NPC> npcs;
 
     /**
      * Zone.
      * @param p1
      * @param p2
      */
-    public void spawnCharacterByParent(final NPC p1, final NPC p2) {
+    public final void spawnCharacterByParent(final NPC p1, final NPC p2) {
         // TODO Auto-generated method stub
+        NPC p3 = new NPC();
+        npcs.add((NPC) p3);
     }
 
 }
