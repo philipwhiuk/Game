6b72b598a9153d5d9707b6a99b99da1a35003fd7
Philip Whitehouse <philip@whiuk.com>
2013-07-09 23:51:37 +0100

    Some outline interfaces for agent sensors.

---
+++ GameServer/src/com/whiuk/philip/mmorpg/server/game/EventListenerSensor.java
@@ -0,0 +1,14 @@
+package com.whiuk.philip.mmorpg.server.game;
+
+import java.util.EventListener;
+
+import com.whiuk.philip.mmorpg.server.game.ai.Sensor;
+
+/**
+ * An implementation of a {@link Sensor} using the {@link EventListener} model.
+ * @author Philip
+ *
+ */
+public interface EventListenerSensor extends Sensor, EventListener {
+
+}
