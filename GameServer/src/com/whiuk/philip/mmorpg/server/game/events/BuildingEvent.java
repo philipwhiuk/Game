895790963e09e0acd10e46a476dc0cd660002663
Philip Whitehouse <philip@whiuk.com>
2013-07-12 18:28:05 +0100

    Implemented framework for BuildingEvent

--- GameServer/src/com/whiuk/philip/mmorpg/server/game/events/BuildingEvent.java
+++ GameServer/src/com/whiuk/philip/mmorpg/server/game/events/BuildingEvent.java
@@ -1,10 +1,29 @@
 package com.whiuk.philip.mmorpg.server.game.events;
 
+import com.whiuk.philip.mmorpg.server.game.domain.Building;
+
 /**
- * 
+ * Provides details on an event that occurred to a Building.
  * @author Philip
  *
  */
 public class BuildingEvent {
+    /**
+     * Building on which the event occurred.
+     */
+    private final Building building;
 
+    /**
+     * @param building Building on which the event occurred
+     */
+    BuildingEvent(final Building building) {
+        this.building = building;
+    }
+
+    /**
+     * @return Building on which the event occurred
+     */
+    public final Building getBuilding() {
+        return building;
+    }
 }
