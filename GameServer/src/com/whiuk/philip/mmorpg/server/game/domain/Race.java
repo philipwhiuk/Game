c7c8039c33f4d3293ac8713e4cc8ab65744ee3e2
Philip Whitehouse <philip@whiuk.com>
2013-07-14 01:53:42 +0100

    New Race class for characters

---
+++ GameServer/src/com/whiuk/philip/mmorpg/server/game/domain/Race.java
@@ -0,0 +1,10 @@
+package com.whiuk.philip.mmorpg.server.game.domain;
+
+public class Race {
+
+    public String getName() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
