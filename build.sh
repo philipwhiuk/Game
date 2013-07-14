756ae1700aa6f030ec2897092638d06119a66432
Philip Whitehouse <philip@whiuk.com>
2013-07-14 04:42:31 +0100

    Build script

---
+++ build.sh
@@ -0,0 +1,10 @@
+cd GameShared
+mvn clean install
+cd ../GameClient
+mvn clean install
+cd ../GameServerShared
+mvn clean install
+cd ../GameServer
+mvn clean install
+cd ../GameWebsite
+mvn clean install
