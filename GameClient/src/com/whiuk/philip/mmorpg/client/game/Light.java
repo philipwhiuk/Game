7acd21132ce714969461ad903a5d4eac894a9bf2
Philip Whitehouse <philip@whiuk.com>
2013-07-29 23:59:16 +0100

    Implement a basic ambient light

--- GameClient/src/com/whiuk/philip/mmorpg/client/game/Light.java
+++ GameClient/src/com/whiuk/philip/mmorpg/client/game/Light.java
@@ -1,16 +1,41 @@
 package com.whiuk.philip.mmorpg.client.game;
 
+import java.nio.FloatBuffer;
+
+import org.lwjgl.BufferUtils;
+import org.lwjgl.opengl.GL11;
+
 /**
  * Light source.
  * @author Philip
  *
  */
 public class Light {
+    private FloatBuffer position;
+    private FloatBuffer model;
+    private FloatBuffer colour;
+
+    /**
+     */
+    Light() {
+        position = BufferUtils.createFloatBuffer(4);
+        position.put(1.0f).put(1.0f).put(10.0f).put(0.0f).flip();
+        colour = BufferUtils.createFloatBuffer(4);
+        colour.put(1.0f).put(1.0f).put(1.0f).put(1.0f).flip();
+        model = BufferUtils.createFloatBuffer(4);
+        model.put(0.5f).put(0.5f).put(0.5f).put(1.0f).flip();
+    }
+
     /**
      *
      */
-    public void render() {
-        // TODO Auto-generated method stub
+    final void render() {
+        GL11.glEnable(GL11.GL_LIGHT0);
+        // sets light position
+        GL11.glLight(GL11.GL_LIGHT0, GL11.GL_POSITION, position);
+        // sets specular light to white
+        GL11.glLight(GL11.GL_LIGHT0, GL11.GL_SPECULAR, colour);
+        GL11.glLight(GL11.GL_LIGHT0, GL11.GL_DIFFUSE, colour);
     }
 
 }
